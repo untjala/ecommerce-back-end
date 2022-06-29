c2faf079dc8544a860fc1e0d94a0805d084240cd
Jala Unterreiner <jala.binbek@outlook.com>
2022-06-29 09:41:42 -0400

    index model relationships established

--- models/index.js
+++ models/index.js
@@ -9,11 +9,19 @@ Product.belongsTo(Category, {
   foreignKey: 'product_id'
 })
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+})
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+})
 module.exports = {
   Product,
   Category,
