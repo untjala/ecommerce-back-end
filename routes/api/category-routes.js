3942bd76432e02995c73e93194b628b99dc59011
Jala Unterreiner <jala.binbek@outlook.com>
2022-06-30 15:11:48 -0400

    added res messages for category and product routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -24,7 +24,7 @@ router.get('/:id', async (req, res) => {
       include: [{model: Product}]
     });
     if (!categoryData) {
-      res.status(404).json({ message: 'No category with this id!' });
+      res.status(404).json({ message: 'No category found with this id!' });
       return;
     }
     res.status(200).json(categoryData);
@@ -37,7 +37,7 @@ router.post('/', async (req, res) => {
   // create a new category
   try {
     const categoryData = await Category.create(req.body);
-    res.status(200).json(categoryData);
+    res.status(200).json({message: 'Category created!'});
   } catch (err) {
     res.status(400).json(err);
   }
@@ -52,10 +52,10 @@ router.put('/:id', async (req, res) => {
       },
     });
     if (!categoryData[0]) {
-      res.status(404).json({ message: 'No category with this id!' });
+      res.status(404).json({ message: 'No category found with this id!' });
       return;
     }
-    res.status(200).json(categoryData);
+    res.status(200).json({message:'Tag updated!'});
   } catch (err) {
     res.status(500).json(err);
   }
@@ -70,10 +70,10 @@ router.delete('/:id', async (req, res) => {
       },
     });
     if (!categoryData) {
-      res.status(404).json({ message: 'No category with this id!' });
+      res.status(404).json({ message: 'No category found with this id!' });
       return;
     }
-    res.status(200).json(categoryData);
+    res.status(200).json({message:'Tag deleted!'});
   } catch (err) {
     res.status(500).json(err);
   }
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -121,7 +121,7 @@ router.delete('/:id', async (req, res) => {
       res.status(404).json({ message: 'No product with this id!' });
       return;
     }
-    res.status(200).json(productData);
+    res.status(200).json({message: 'Product deleted!'});
   } catch (err) {
     res.status(500).json(err);
   }
